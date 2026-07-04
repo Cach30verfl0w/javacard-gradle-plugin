bfa0c50932b3ed5cb55f79cb9141a84c07a38ecb
cach30verfl0w <contact@cach30verfl0w.net>
2026-07-04 18:23:03 +0200

    feat: Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "javacard-gradle-plugin"
+include(":javacard-gradle-plugin")
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
